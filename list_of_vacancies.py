1e124a57a1643c721261211c7ec725171f2c7ae1
Ash2803 <ashot.93@mail.ru>
2022-10-03 20:22:43 +0400

    Refactored get_vacancies()

--- list_of_vacancies.py
+++ list_of_vacancies.py
@@ -3,92 +3,70 @@ from pprint import pprint
 import requests
 
 
-def get_vacancies():
+def get_vacancies(language):
     """Getting vacancies from HeadHunter"""
-    languages = [
-        'Python',
-        'Java',
-        'Javascript',
-        'Ruby',
-        'PHP',
-        'C++',
-        'C#',
-        'C',
-        'Scala',
-        'Swift'
-    ]
-    for language in languages:
-        params = {
-            'text': f'Программист {language}',
-            'area': '1',
-            'only_with_salary': 'true'
-        }
-        response = requests.get('https://api.hh.ru/vacancies', params=params)
-        response.raise_for_status()
-        return response.json()['items']
+    params = {
+        'text': f'Программист {language}',
+        'area': '1',
+        'only_with_salary': 'true'
+    }
+    response = requests.get('https://api.hh.ru/vacancies', params=params)
+    response.raise_for_status()
+    return response.json()['items']
 
 
-def get_vacancies_count():
-    languages = [
-        'Python',
-        'Java',
-        'Javascript',
-        'Ruby',
-        'PHP',
-        'C++',
-        'C#',
-        'C',
-        'Scala',
-        'Swift'
-    ]
+def get_vacancies_count(language):
     popular_langs = {}
-    for language in languages:
-        params = {
-            'text': language,
-            'area': '1',
-        }
-        response = requests.get('https://api.hh.ru/vacancies', params=params)
-        response.raise_for_status()
-        popular_langs[language] = response.json()['found']
+    params = {
+        'text': language,
+        'area': '1',
+    }
+    response = requests.get('https://api.hh.ru/vacancies', params=params)
+    response.raise_for_status()
+    popular_langs[language] = response.json()['found']
     return popular_langs
 
 
 def predict_rub_salary(vacancies):
     for salary in vacancies:
         if salary['salary']['currency'] != 'RUR':
-            print(None)
+            return None
         if salary['salary']['from'] and salary['salary']['to']:
             avg_salary = (salary['salary']['from'] + salary['salary']['to']) / 2
-            print(int(avg_salary))
         elif salary['salary']['from']:
             from_avg_salary = salary['salary']['from'] * 1.2
-            print(int(from_avg_salary))
         elif salary['salary']['to']:
             to_avg_salary = salary['salary']['to'] * 0.8
-            print(int(to_avg_salary))
-            # pprint(salary['salary'])
-    # print(vacancies[0]['salary'])
+        return to_avg_salary
 
 
 def get_salary():
     params = {
         'text': 'Python',
         'area': '1',
-        'salary': '100000',
         'only_with_salary': 'true'
     }
     response = requests.get('https://api.hh.ru/vacancies', params=params)
     response.raise_for_status()
     for i in response.json()['items']:
-        return i['salary']
+        pprint(i['salary'])
 
 
 def main():
-    pprint(get_vacancies())
-    # predict_rub_salary(get_vacancies())
-    # # pprint(get_vacancies())
-    # # pprint(get_vacancies_count())
-    # # pprint(get_salary())
+    languages = [
+        'Python',
+        'Java',
+        'Javascript',
+        'Ruby',
+        'PHP',
+        'C++',
+        'C#',
+        'C',
+        'Scala',
+        'Swift'
+    ]
+    for language in languages:
+        pprint(predict_rub_salary(get_vacancies(language)))
 
 
 if __name__ == '__main__':
