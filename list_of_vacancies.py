942c265355d5795a1583b0e7bc7fe9e9dafeed48
Ash2803 <ashot.93@mail.ru>
2022-10-09 17:00:27 +0400

    added superjob statistics

--- list_of_vacancies.py
+++ list_of_vacancies.py
@@ -27,29 +27,69 @@ import requests
 #     return vacancies
 
 
-def get_vacancies_sj(secret_key):
+def get_vacancies_sj(secret_key, language):
+    page = 0
+    pages_number = 1
+    header = {
+        'X-Api-App-Id': secret_key
+    }
+    vacancies = []
+    while page < pages_number:
+        params = {
+            'town': 4,
+            'catalogues': 33,
+            'keyword': f'Программист {language}',
+            'no_agreement': 1,
+            'page': page
+            }
+        response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=header, params=params)
+        response.raise_for_status()
+        page += 1
+        pages_number += 1
+        if not response.json()['objects']:
+            break
+        else:
+            vacancies.extend(response.json()['objects'])
+            return vacancies
+
+
+def predict_rub_salary_sj(vacancy):
+    predict_salaries = []
+    for salary in vacancy:
+        if salary['currency'] != 'rub':
+            continue
+        if salary['payment_from'] and salary['payment_to']:
+            predict_salaries.append((salary['payment_from'] + salary['payment_to']) // 2)
+        if salary['payment_from']:
+            predict_salaries.append(int(salary['payment_from'] * 1.2))
+        if salary['payment_to']:
+            predict_salaries.append(int(salary['payment_to'] * 0.8))
+    return predict_salaries
+
+
+def get_vacancies_count_hh(language):
+    params = {
+        'text': f'Программист {language}',
+        'area': '1',
+    }
+    response = requests.get('https://api.hh.ru/vacancies', params=params)
+    response.raise_for_status()
+    return response.json()['found']
+
+
+def get_vacancies_count_sj(secret_key, language):
     header = {
         'X-Api-App-Id': secret_key
     }
     params = {
         'town': 4,
         'catalogues': 33,
-        'keyword': 'Программист'
-    }
+        'keyword': f'Программист {language}',
+        'no_agreement': 1
+        }
     response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=header, params=params)
     response.raise_for_status()
-    for profession in response.json()['objects']:
-        print(profession['profession'], profession['town']['title'], sep=", ")
-
-
-def get_vacancies_count(language):
-    params = {
-        'text': language,
-        'area': '1',
-    }
-    response = requests.get('https://api.hh.ru/vacancies', params=params)
-    response.raise_for_status()
-    return response.json()['found']
+    return response.json()['total']
 
 
 # def predict_rub_salary(vacancies):
@@ -66,9 +106,9 @@ def get_vacancies_count(language):
 #     return predict_salaries
 
 
-# def get_avg_salary(salaries):
-#     avg_salary = round(sum(salaries) / len(salaries))
-#     return avg_salary
+def get_avg_salary(salaries):
+    avg_salary = round(sum(salaries) / len(salaries))
+    return avg_salary
 
 
 # def get_salary():
@@ -86,20 +126,28 @@ def get_vacancies_count(language):
 def main():
     load_dotenv()
     secret_key = os.environ['SECRET_KEY']
-    get_vacancies_sj(secret_key)
+    # a = get_vacancies_sj(secret_key)
+    # print(predict_rub_salary_sj(a))
+    languages = [
+        'Python',
+        'Java',
+        'Javascript',
+        'Ruby',
+        'PHP',
+        'C++',
+        'C#',
+        'C',
+        'Scala',
+        'Swift'
+    ]
+    dict_ = {}
+    for language in languages:
+        a = predict_rub_salary_sj(get_vacancies_sj(secret_key, language))
+        dict_[language] = {'vacancies_found': get_vacancies_count_sj(secret_key, language),
+                           'vacancies_processed': len(a),
+                           "average_salary": get_avg_salary(a)}
+    pprint(dict_)
 
-    # languages = [
-    #     'Python',
-    #     'Java',
-    #     'Javascript',
-    #     'Ruby',
-    #     'PHP',
-    #     'C++',
-    #     'C#',
-    #     'C',
-    #     'Scala',
-    #     'Swift'
-    # ]
     # dict_ = {}
     # for language in languages:
     #     a = predict_rub_salary(get_vacancies(language))
